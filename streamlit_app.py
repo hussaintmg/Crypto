1acbdc14c8641866f08703241ebc9744bd23a15e
Hussain <hussaintmerng@gmail.com>
2026-03-10 02:33:55 +0500

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,19 @@ import streamlit as st
 import sys
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(ROOT_DIR)
-sys.path.append(os.path.join(ROOT_DIR, "src"))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from src.dashboard import CryptoDashboard
 from src.etl_pipeline import ETLPipeline
 
 
 def main():
-    pipeline = ETLPipeline()
-    pipeline.run_etl()
+
+    # Run ETL once
+    if "etl_started" not in st.session_state:
+        pipeline = ETLPipeline()
+        pipeline.run_etl()
+        st.session_state.etl_started = True
 
     dashboard = CryptoDashboard()
     dashboard.run()
